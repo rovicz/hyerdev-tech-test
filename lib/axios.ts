4bb3a23a22128720356669f24ca1aa4119f16d85
rovicz <roviczzf@gmail.com>
2026-02-12 17:27:36 -0300

    feat: creating axios api

---
+++ lib/axios.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import https from "https";
+
+const api = axios.create({
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept":
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+  },
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+});
+
+export default api;
